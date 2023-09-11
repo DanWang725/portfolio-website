dc01ed8aa4e8e435d90a3c06d0907a3a456d26a9
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2023-09-11 19:13:29 -0400

    added temp content and some styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import { Navbar } from "./navbar/Navbar";
 import { AppRoutes } from "./Routes";
 import { BrowserRouter } from "react-router-dom";
-import React from "react";
+import React, { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    window.addEventListener(
+      "scroll",
+      () => {
+        document.body.style.setProperty(
+          "--scroll",
+          window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+        );
+      },
+      false
+    );
+  }, []);
   return (
     <BrowserRouter>
       <div className="App">
-        <Navbar />
+        <Navbar disableAtHome={true} />
         <AppRoutes />
       </div>
     </BrowserRouter>
--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,53 +1,60 @@
 import "./Navbar.css";
 import "../styles.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import React from "react";
 
-export function Navbar() {
+export const Navbar = ({ disableAtHome = false, extraClasses = "" }) => {
+  const location = useLocation();
   return (
-    <div className="nav-container">
-    <ul className="navbar">
-      <li>
-        <NavLink to="/" className="navlink" style={({ isActive }) =>
-            isActive ? { backgroundColor: "#cbcbcb" } : {}
-          }>
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/browse"
-          className="navlink"
-          style={({ isActive }) =>
-            isActive ? { backgroundColor: "#cbcbcb" } : {}
-          }
-        >
-          About
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/create"
-          className="navlink"
-          style={({ isActive }) =>
-            isActive ? { backgroundColor: "#cbcbcb" } : {}
-          }
-        >
-          Create
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/collection"
-          className="navlink"
-          style={({ isActive }) =>
-            isActive ? { backgroundColor: "#cbcbcb" } : {}
-          }
-        >
-          My Collection
-        </NavLink>
-      </li>
-    </ul>
-    </div>
+    (!disableAtHome || location.pathname !== "/") && (
+      <div className={`nav-container ${extraClasses}`}>
+        <ul className="navbar">
+          <li>
+            <NavLink
+              to="/"
+              className="navlink"
+              style={({ isActive }) =>
+                isActive ? { backgroundColor: "#cbcbcb" } : {}
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/browse"
+              className="navlink"
+              style={({ isActive }) =>
+                isActive ? { backgroundColor: "#cbcbcb" } : {}
+              }
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/create"
+              className="navlink"
+              style={({ isActive }) =>
+                isActive ? { backgroundColor: "#cbcbcb" } : {}
+              }
+            >
+              Create
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/collection"
+              className="navlink"
+              style={({ isActive }) =>
+                isActive ? { backgroundColor: "#cbcbcb" } : {}
+              }
+            >
+              My Collection
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    )
   );
-}
+};
