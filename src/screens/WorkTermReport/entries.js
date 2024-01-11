d0358101e57dedc79c8306f1bb53cf8105af967b
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2024-01-11 18:57:44 -0500

    Add entry for second report

--- src/screens/WorkTermReport/entries.js
+++ src/screens/WorkTermReport/entries.js
@@ -4,7 +4,7 @@ const workTermReport1 = {
   title: "Work Term Report S23",
   id: "wtr-s23",
   image: cover1,
-  description: "This covers my first 4 months at NCR",
+  description: "Report for the first half of my NCR placement",
   entries: [
     {
       title: "Introduction",
@@ -54,15 +54,69 @@ const workTermReport1 = {
 };
 
 const workTermReport2 = {
-  title: "Work Term Report 2",
+  title: "Work Term Report W23",
   id: "wtr-w23",
   image: cover2,
-  description: "This is the second work term report.",
+  description: "Report for the second half of my NCR placement.",
   entries: [
     {
       title: "Introduction",
       id: "introduction",
-      content: "TBA",
+      content:
+        "This work term report goes over the second half of my 8-month work term as a front-end developer at NCR. My experience working throughout this half continued to be amazing, and I had many challenging but fulfilling tasks I worked on. Within this report, I will talk about the major company split that occurred. I will also describe the goals I set for this term and how my work experience has changed from the first half, including some notable features I worked on. I will also talk about how I got a patent from the company hackathon.",
+    },
+    {
+      title: "Company Split",
+      id: "company-split",
+      content:
+        "Around October, NCR officially split into two new companies: NCR Atleos and NCR Voyix. These new companies would specialize in NCR’s two main products or services: Atleos covering the ATM businesses and Voyix covering digital commerce. These two companies were independent of each other. Voyix was the legal successor to NCR Corporation, while Atleos was spun off. Currently, Voyix offers a variety of services focused on software in three main areas: retail, restaurants, and digital banking.",
+    },
+    {
+      title: "Goals",
+      id: "goals",
+      content: `Moving into the second half of this work term, I had a decision to make: whether to try taking on some back-end development tasks or to dedicate my learning to front-end development. I felt that I had learned a good deal about front-end development at the halfway point to consider trying something new. Switching to a full-stack developer role, I could also learn about the back-end codebase. In the end, I decided that the best choice would be to continue focusing on front-end development. Since this was my first co-op work term, I still had two chances to try out the backend. This was my chance to learn and experience in-depth front-end development. To go along with this decision, I created the following goals to suit my decision:
+      
+      My first goal was to learn the rest of JavaScript and React. Despite the amount of work I had done in the first four months, there were still some areas of React that I had not touched fully upon. After discussing with my team lead, I still needed to work with React providers, asynchronous functions and promises, and advanced JavaScript object/array methods. I was not sure how long this goal would take to achieve since it was dependent on what my team was working on. However, I finished it quickly, since many of the topics I lacked knowledge on I quickly learned after working on a story that used them or reading up on that topic.
+      
+      Next, I also wanted to improve my clarity and organization when presenting demos or conducting reviews. While my explanations and demos were sufficient in the previous sprint, they were far from great. I noticed that while I would be explaining a complex component that has many features, I struggled to describe how it functions in a concise way. I would also benefit from improving my communication skills early, since being able to write and explain code is an important skill for a software developer in general. Over the course of the work term, I would use strategies to prepare my talking points prior to demos or reviews, and gradually I noticed improvements in clarity. For my last demo during the sprint review, I demoed all my team’s front-end stories without any issues.
+      
+      In addition, I wanted to develop safer coding techniques when adding to existing code. Considering that the application I worked on was mostly finished and my team would be developing additional features, it was important to prevent accidental changes to the existing code. During the first half of my work term, I did not consider the impact of my changes on existing functionality. As I began to take on larger tasks, the significance of preserving the existing implementation became more apparent. Some components within the application contained bad code that did not follow our conventions. Initially, I often tried to clean it up but kept the same functionality. However, making improvements such as that was not that beneficial to an app that was already released to customers who wanted to keep it the same. Only code that was directly within the scope of the story would warrant making changes. As I was given larger stories to work on, I started to recognize what code could be changed.
+      
+      Finally, I wanted to learn more about how the app was designed for large, codebase-wide components and conventions. I felt it was important to have knowledge of good code design within an app, since inevitably some developers would need to make changes later. In addition, with the amount of work I did developing components this term, being able to create good designs was a must. Having experience would also inform me of any design patterns to avoid, such as adding functions that fulfill more than one purpose. This was the most challenging goal to achieve since good design is always subjective. However, the developer process that we follow when working on features contains many steps to get reviews and opinions from other developers. Being able to get a second opinion helps a lot, and I often checked my ideas with my team lead or during design reviews.
+      `,
+    },
+    {
+      title: "Job Description",
+      id: "job-description",
+      content: `My placement at NCR had me continue working as a front-end developer with my team from the first portion of the term. Our team was still a feature team, which meant that we were developing new additional features for our bank teller platform. Since our team was moved to work under a new customer, the features we developed were less connected to the existing code and would be entirely new user flows. In addition to these features, I worked on a wide variety of other areas within the app. There were some days, or even sprints, where bugs dominated much of our work capacity. Some days, all of us front-end developers would be trying to merge two development branches together to sync changes.
+      
+      With 4 months of experience, I felt integrated with my team and my role, and I was ready to start working on larger and more complex stories. I felt much more capable and familiar with the application now. During sprint planning and implementation reviews, I actively participated and suggested my own ideas and thoughts.
+      
+      During this term, I was able to work on some notable features, such as creating a new screen. Developing this screen required an understanding of how the app renders its routes and the other properties required to properly show this screen. In addition, one of the last features I worked on was a generic transaction editor. This editor would be used for all future transactions with our customer, and it needed to be both generic and handle all the necessary API calls that other editors do. Like the screen component, I needed to understand how other existing editors within the app work and what properties they require, as well as what API endpoint they would be calling. Both components were very interesting to work on, and it was exciting to learn about how everything was organized and integrated with the editors.
+      
+      In addition to new features, I was able to contribute to the improvement of the existing application. During one of my investigations, I noticed that one of the large forms in the app called a performance-heavy function hundreds of times per input, slowing the user experience by 80%. I was given the okay to refactor this issue and reduce the number of calls to single digits. As a result, the developer and user experience were greatly improved, as it felt more responsive to work with. This investigation also identified the issue existing throughout the entire app. I was able to help my team improve performance throughout the entire app. I also learned a lot from this investigation. I wanted to understand the cause of this suboptimal behaviour, and I ended up learning how forms from the Formik library work. I gained an understanding of how React itself performs updates and re-renders through the DOM, the document object model, where the HTML elements are stored.
+      
+      The skills that were important to my job are the same as those listed in my first work term report: web development languages including HTML, CSS, and React; development tools like GitHub and Jira; and the agile development methodology. More importantly, however, is the mindset when working here. I always took any chance to challenge myself and accomplish as much as I could, such as taking the initiative to look for work and advocating ideas during meetings, to name some examples. My team was more than happy to accept and encourage me to do challenging work, which is how I was able to gain and learn all the skills I have now.
+      `,
+    },
+    {
+      title: "NCR Hackathon",
+      id: "hackathon",
+      content: `In addition to all my accomplishments during my work term, I got a patent filed with my name on it. In the first half of my work term in August, our company hosted a company-wide hackathon that would last two days. My team, called Eason and Friends, was a group of seven Guelph co-ops. We came up with the idea of using blockchain as a medium for interbank transfers. As a quick rundown on how blockchain networks like Ethereum work in general, they function using smart contracts. These contracts basically have their programming hard-coded into the blockchain. When anyone performs a transaction (as in running the smart contract), the entire procedure is public, as everyone can see and verify the code run. The most common language to use to write these is Solidity, which was a new language to everyone on the team. I took the initiative, learning and then creating a basic smart contract in Solidity over the two days. I also helped my team setup a local blockchain using my contract and helped develop a front end that would connect to the blockchain. At the end of the hackathon, we had a fully working prototype of our idea consisting of a front-end React application, a Java back-end, and a blockchain network. Although we didn’t win any prizes, the hackathon was a blast! It was fun working together as a team for over 12 hours at the office.
+      
+      The great thing about the company hackathon was that many ideas had the potential to become actual applications (one of NCR's platforms started out as a hackathon idea). It was a welcome surprise to see that the application we had sent in had passed all rounds of judging and that our idea would be patented. The process of getting the patent created took months, and the updates were slow. By the time our lawyer had a draft for us to approve, most of us were finished with this term. In fact, when this report was written in January, the patent had not yet been filed. However, it is amazing that, as a team, we managed to get a patent filed for our idea.
+      `,
+    },
+    {
+      title: "Conclusion",
+      id: "conclusion",
+      content: `Choosing NCR as my first co-op placement has been an amazing opportunity. I have grown so much in experience and knowledge since I started my placement. It feels unreal that, by the end, I was being assigned work equal to that of a regular developer. I was presented with challenging but fulfilling work that greatly benefited my learning experience. Reflecting on all the things I have done, it is amazing how much I was able to do during my term and the impact my code has on the current application. Aside from the work I did, the people I worked with and interacted with were outstanding. Everyone at NCR was very helpful and would usually take some time to assist me with my issue when I asked. My team was always looking out for my success and was supportive in many ways, making this experience unforgettable. I hope that I can come back to work at NCR in the future!`,
+    },
+    {
+      title: "Acknowledgements",
+      id: "acknowledgements",
+      content:
+        "My experience at NCR can be attributed to the awesome team I worked with. Big thanks to my team leader, Lubna, for supporting me throughout my placement and helping me improve in my weaker areas, as well as to my entire team for being fun to work with and making this first work term so memorable. Finally, I want to thank some of the other co-ops (Eason, Mark, Ethan, Myron, Isaiah, and Max) for being great to hang out with and keeping the $2 burger tradition.",
     },
   ],
 };
