3797a89e0daa26bb315fad05beb2f9c83d0ca297
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2024-04-18 20:43:09 -0400

    update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,52 @@
 module.exports = {
+  plugins: ['jest'],
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parser: '@babel/eslint-parser',
+
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: "script",
+        sourceType: 'script',
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: '6',
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
   },
-  plugins: ["react"],
   rules: {
-    "react/prop-types": "off",
-    "react/react-in-jsx-scope": "off",
-    "no-unused-vars": "warn",
+    quotes: [2, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
+    'no-unused-vars': ['warn', { vars: 'all' }],
+    'react/react-in-jsx-scope': 0,
+    'react/destructuring-assignment': 2,
+    'react/function-component-definition': [
+      2,
+      { namedComponents: 'arrow-function' },
+    ],
+    'react/prop-types': 0,
+    'no-restricted-imports': [
+      2,
+      {
+        name: 'axios',
+        message:
+          'Imports of axios package is forbidden. Pwease use axios from /shared-components/axios',
+      },
+    ],
   },
 };
