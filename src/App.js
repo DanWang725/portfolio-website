454acbec6848278aa6ae128e2fd5cf03412b7bec
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2024-04-26 15:01:38 -0400

    add toaster to providers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import { Navbar } from './navbar/Navbar';
-import { AppRoutes } from './Routes';
 import { HashRouter } from 'react-router-dom';
-import React, { createContext, useCallback, useEffect, useState } from 'react';
-import Footer from './components/Footer';
+import React, { useState } from 'react';
 import Body from './screens/Body';
-import { loadFull } from 'tsparticles';
-import { loadSlim } from 'tsparticles-slim'; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
-import Particles from 'react-particles';
-import { scroll } from 'scroll-speed';
 import ParticleWrapper from './screens/ParticleWrapper/ParticleWrapper';
 import './styles.css';
 import { PerformanceContext } from './Contexts';
 import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
+import { Toaster } from 'react-hot-toast';
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
@@ -27,6 +21,7 @@ const App = () => {
       >
         {!isLowPerformance && <ParticleWrapper />}
         <CssBaseline />
+        <Toaster />
         <HashRouter>
           <Body></Body>
         </HashRouter>
