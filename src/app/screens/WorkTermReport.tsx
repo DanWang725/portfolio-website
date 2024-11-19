931a82bf11c9c6beb06387d70c752d7cb59833a4
DanWang725 <dw.smaug@gmail.com>
2024-11-19 12:25:18 -0500

    update styling for the homepage of workterm reports

--- src/app/screens/WorkTermReport.tsx
+++ src/app/screens/WorkTermReport.tsx
@@ -4,6 +4,8 @@ import ArticleSidebar from '../../components/Article/ArticleSidebar';
 import { workTermReportEntries } from '../../features/Articles/entries';
 import WorkTermReportCard from '../../features/Articles/WorkTermReportCard';
 import { useNavigate, useParams } from 'react-router-dom';
+import ContentSection from '../../components/Sections/ContentSection';
+import { List } from '@mui/material';
 
 const WorkTermReport = () => {
   const params = useParams();
@@ -13,38 +15,45 @@ const WorkTermReport = () => {
     workTermReportEntries.find((article) => article.id === params.reportId) ||
     null;
 
+  if (selectedArticle) {
+    return (
+      <ArticleLayout
+        classOverrides={'no-title'}
+        article={selectedArticle}
+        handleBack={() => navigate('/work-term-report')}
+      />
+    );
+  }
+
   return (
-    <div className="content-section">
-      {selectedArticle ? (
-        <ArticleLayout
-          classOverrides={'no-title'}
-          article={selectedArticle}
-          handleBack={() => navigate('/work-term-report')}
-        />
-      ) : (
-        <>
-          <section className="std-container work-report-container">
-            <h1 className="article-title">Work Term Reports</h1>
-            <p className="article-content">
-              These reports are cover my experiences and achievements during my
-              co-op work terms. 8-month work terms are split into two separate
-              reports covering 4 months each.
-            </p>
-            <div className="report-list">
-              {workTermReportEntries.map((article) => (
-                <WorkTermReportCard
-                  workTermReportEntry={article}
-                  setSelectedWtr={(entry) =>
-                    navigate(`/work-term-report/${entry.id}`)
-                  }
-                  key={article.title.toLowerCase()}
-                />
-              ))}
-            </div>
-          </section>
-        </>
-      )}
-    </div>
+    <ContentSection
+      styleOverrides={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: '1rem',
+      }}
+    >
+      <h1 className="article-title">Work Term Reports</h1>
+      <p className="article-content">
+        These reports are cover my experiences and achievements during my co-op
+        work terms. 8-month work terms are split into two separate reports
+        covering 4 months each.
+      </p>
+      <div className="report-list">
+        <List>
+          {workTermReportEntries.map((article) => (
+            <WorkTermReportCard
+              workTermReportEntry={article}
+              setSelectedWtr={(entry) =>
+                navigate(`/work-term-report/${entry.id}`)
+              }
+              key={article.title.toLowerCase()}
+            />
+          ))}
+        </List>
+      </div>
+    </ContentSection>
   );
 };
 export default WorkTermReport;
