8e89d186612098b7f00860ecf1886976161c933e
DanWang725 <dw.smaug@gmail.com>
2024-12-11 14:20:11 -0500

    add fps counter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,14 +5,16 @@ import PerformanceProvider, {
 import ParticleWrapper from '../features/ParticleBackground/ParticleWrapper';
 import { Navbar } from './navbar/Navbar';
 import { Container } from '@mui/material';
+import FPSStats from 'react-fps-stats';
 
 const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const { isLowPerformance } = useContext(PerformanceContext);
+  const { isLowPerformance, isDebugMode } = useContext(PerformanceContext);
   return (
     <>
       {!isLowPerformance && <ParticleWrapper />}
       <Navbar />
       <Container sx={{ mt: '80px' }}>{children}</Container>
+      {isDebugMode && <FPSStats />}
     </>
   );
 };
