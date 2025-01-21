aa339cb8334c794061db8eccef12de122bbf471e
DanWang725 <dw.smaug@gmail.com>
2025-01-20 19:06:47 -0500

    Add a player class

---
+++ src/features/Risk/battles/Player.ts
@@ -0,0 +1,38 @@
+import { diceStats } from '../components/utils';
+import { DiceRoll, DiceRoller } from '../dice/DiceRoller';
+import { DiceRollStats } from '../dice/DiceStats';
+
+export class Player {
+  diceStats: DiceRollStats;
+  initialTroops: number;
+  troops: number;
+  constructor(roller: DiceRoller, initialTroops: number) {
+    this.diceStats = new DiceRollStats(roller);
+    this.initialTroops = initialTroops;
+    this.troops = initialTroops;
+  }
+
+  public getTroops(): number {
+    return this.troops;
+  }
+
+  public getInitialTroops(): number {
+    return this.initialTroops;
+  }
+
+  public getDiceStats(): number[] {
+    return this.diceStats.getStats();
+  }
+
+  public getRolls(): DiceRoll[] {
+    const maxRolls = Math.min(this.troops, 3);
+    return Array(maxRolls).map(() => this.diceStats.getRoll());
+  }
+
+  public removeTroops(amount: number): void {
+    this.troops -= amount;
+  }
+  public canBattle(): boolean {
+    return this.troops > 1;
+  }
+}
