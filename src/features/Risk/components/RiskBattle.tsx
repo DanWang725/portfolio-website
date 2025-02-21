39cdfffda047ca6eb0bafbeae32a0827e9786763
DanWang725 <dw.smaug@gmail.com>
2025-02-21 16:06:23 -0500

    prototype battle screen

--- src/features/Risk/components/RiskBattle.tsx
+++ src/features/Risk/components/RiskBattle.tsx
@@ -1,11 +1,11 @@
 import ContentSection from '@components/Sections/ContentSection';
-import { Button, Typography } from '@mui/material';
+import { Box, Button, Grid2, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { Battle, BattleStatus } from '../battles/Battles';
 import useRiskBattleManager from '../battles/useRiskBattleManager';
 
 const RiskBattle: React.FC = () => {
-  const { attacker, defender, battleStatus, init, playRound, rounds } =
+  const { attacker, defender, battleStatus, init, playRound, rounds, seed } =
     useRiskBattleManager();
 
   const handleStart = () => {
@@ -19,29 +19,48 @@ const RiskBattle: React.FC = () => {
           Start
         </Button>
       )}
-      {battleStatus === BattleStatus.Ongoing && (
-        <>
-          <p>Attacker Troops: {attacker?.troops}</p>
-          <p>Defender Troops: {defender?.troops}</p>
-          <Button variant="contained" onClick={() => playRound()}>
-            Play Round
-          </Button>
-          {rounds.map((round, index) => {
-            return (
-              <div key={`round-${index}`}>
-                <Typography>Round {index + 1}</Typography>
-                <Typography>
-                  Attacker: {round.attackerRolls.join(', ')}
-                </Typography>
-                <Typography>
-                  Defender: {round.defenderRolls.join(', ')}
-                </Typography>
-                <Typography>Attacker Losses: {round.attackerLosses}</Typography>
-                <Typography>Defender Losses: {round.defenderLosses}</Typography>
-              </div>
-            );
-          })}
-        </>
+      {battleStatus !== BattleStatus.NotStarted && (
+        <Grid2 container>
+          <Grid2
+            size={{ xs: 12, md: 4 }}
+            sx={{ border: '1px solid black', width: '30%' }}
+          >
+            <p>{seed}</p>
+            <p>Attacker Troops: {attacker?.troops}</p>
+            <p>Defender Troops: {defender?.troops}</p>
+            {battleStatus === BattleStatus.Ongoing && (
+              <Button variant="contained" onClick={() => playRound()}>
+                Play Round
+              </Button>
+            )}
+          </Grid2>
+          <Grid2
+            size={{ xs: 12, md: 8 }}
+            key={`rounds`}
+            sx={{
+              border: '1px solid black',
+              height: '50vh',
+              width: '100%',
+              overflow: { xs: 'auto', md: 'scroll' },
+            }}
+          >
+            {rounds.map((round, index) => {
+              return (
+                <Box key={`round-${index}`} mb="1rem">
+                  <Typography>Round {index + 1}</Typography>
+                  <Typography>
+                    Attacker: {round.attackerRolls.join(', ')}
+                    Attacker Losses: {round.attackerLosses}
+                  </Typography>
+                  <Typography>
+                    Defender: {round.defenderRolls.join(', ')}
+                    Defender Losses: {round.defenderLosses}
+                  </Typography>
+                </Box>
+              );
+            })}
+          </Grid2>
+        </Grid2>
       )}
       <Typography>{battleStatus}</Typography>
       <Typography>{rounds.length}</Typography>
