3b16f7bb935231b0f384012df1ff5d7ddc2789e7
DanWang725 <dw.smaug@gmail.com>
2025-09-07 13:28:12 -0400

    refactor types

--- src/components/Article/types/article.ts
+++ src/components/Article/types/article.ts
@@ -1,9 +1,9 @@
 import { Breakpoint } from '@mui/material';
 import { SxProps, Theme } from '@mui/system';
 
-export enum SectionType {
+export enum EntryType {
   TEXT = 'text',
-  CUSTOM = 'custom',
+  HEADER = 'header',
 }
 
 export interface Article {
@@ -11,7 +11,7 @@ export interface Article {
   id: string;
   image?: string;
   description: string;
-  entries: ArticleSection[];
+  entries: ArticleEntry[];
 }
 
 /**
@@ -24,18 +24,27 @@ interface BaseArticleSection {
   id: string;
 }
 
-export interface TextSection extends BaseArticleSection {
-  content: string;
-  type: SectionType.TEXT;
+interface BaseArticleEntry {
+  id?: string;
+  content: any;
 }
 
-export interface CustomArticleSection extends BaseArticleSection {
-  content: Sections[];
-  gap?: number;
-  type: SectionType.CUSTOM;
+/** A text article entry with no header */
+interface TextArticleEntry extends BaseArticleEntry {
+  type: EntryType.TEXT;
+  content: string | Sections[];
 }
 
-export type ArticleSection = TextSection | CustomArticleSection;
+/** A article entry with a header included. Include a short title to have it appear in the sidebar */
+interface HeaderArticleEntry extends BaseArticleEntry {
+  id: string;
+  type: EntryType.HEADER;
+  content: string | Sections[];
+  title: string;
+  titleShort?: string;
+}
+
+export type ArticleEntry = TextArticleEntry | HeaderArticleEntry;
 
 export interface ContentGrid {
   size: number | { [key in Breakpoint]?: number };
@@ -45,17 +54,32 @@ export interface ContentGrid {
 export enum ContentType {
   IMAGE = 'image',
   TEXT = 'text',
-}
-export interface ImageContent {
-  /** Image content for the article, requires src and alt properties */
-  type: ContentType.IMAGE;
-  src: string;
-  alt: string;
+  LIST = 'list',
 }
 
-export interface TextContent {
+interface BaseContent {
+  /** Type of content, can be image, text or list */
+  type: ContentType;
+  value: any;
+}
+export interface ImageContent extends BaseContent {
+  /** Image content for the article, requires src and alt properties */
+  type: ContentType.IMAGE;
+  value: {
+    src: string;
+    alt: string;
+  };
+}
+
+export interface TextContent extends BaseContent {
   /** Text content for the article, requires text property */
   type: ContentType.TEXT;
-  text: string;
+  value: string;
 }
-export type Sections = ContentGrid & (ImageContent | TextContent);
+
+export interface ListContent extends BaseContent {
+  /** List content for the article, requires items property */
+  type: ContentType.LIST;
+  value: string[];
+}
+export type Sections = ContentGrid & BaseContent;
