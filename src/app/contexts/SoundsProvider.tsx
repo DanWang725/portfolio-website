725791ae4a48ed11da81e1e9c7c576d350d99194
DanWang725 <dw.smaug@gmail.com>
2025-09-28 17:59:53 -0400

    finish refactor of the sounds provider, move it to a hook based component

--- src/app/contexts/SoundsProvider.tsx
+++ src/app/contexts/SoundsProvider.tsx
@@ -1,91 +1,172 @@
-import { createContext, ReactNode, useContext, useRef } from 'react';
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { TimeoutContext, TimeoutManager } from './TimeoutProvider';
-/** Represents a random sound timeout */
-export interface RandomSound {
-  timeoutId: number;
-  id: number;
-  /** Represents the media type url to play when triggered */
-  url: string;
-  /** Represents the interval set when sound was queued. */
-  initialTimeout: number;
-  /** Represents the maximum time can go */
-  upperBound: number;
-}
+import { useImmer } from 'use-immer';
+import { RandomSound, SoundsProviderValues } from '../../types/Contexts';
 
-/** Represents a random sound which has been paused */
-export interface RandomSoundPaused extends RandomSound {
-  remainingTime: number;
-}
+const UseRandomSounds = (timeoutManager: TimeoutManager) => {
+  const [loadedSounds, updateLoadedSounds] = useImmer<RandomSound[]>([]);
+  const [toPlay, setToPlay] = useState<number[]>([]);
 
-class RandomSoundsManager {
-  private randomSoundsPlaying: RandomSound[] = [];
-  private randomSoundsPaused: RandomSoundPaused[] = [];
-  private _timeoutManager: TimeoutManager;
-  public constructor(timeoutManager: TimeoutManager) {
-    this._timeoutManager = timeoutManager;
-  }
+  useEffect(() => {
+    if (toPlay.length === 0) {
+      return;
+    }
 
-  private getSound(soundId: number) {
-    return this.randomSoundsPlaying.find((snd) => snd.id == soundId);
-  }
-  private onSoundPlay(soundId: number) {
-    const sound = this.randomSoundsPlaying.find((snd) => snd.id == soundId);
+    toPlay.forEach((element) => {
+      onSoundPlay(element);
+    });
+    setToPlay([]);
+  }, [toPlay]);
+
+  const queuePlay = (soundId: number) => {
+    setToPlay((val) => [...val, soundId]);
+  };
+
+  const onSoundPlay = (soundId: number) => {
+    console.log('triggered for ', soundId);
+    const sound = loadedSounds.find((snd) => snd.id == soundId);
     if (sound == null) {
+      console.log('could not find sound for ', soundId, loadedSounds);
       return;
     }
 
-    const newTimeout = this.getTimeout(sound);
-    const newId = this._timeoutManager.setTimeout(() => {
-      this.onSoundPlay(soundId);
-    }, this.getTimeout(sound));
-    sound.initialTimeout = newTimeout;
-    sound.timeoutId = newId;
-  }
+    const newTimeout = getTimeout(sound.upperBound);
+    const timeoutId = startSound(sound.id, newTimeout);
+    const audio = new Audio(sound.url);
+    console.log(
+      'playing audio',
+      sound.timeoutId,
+      ' => ',
+      timeoutId,
+      `(${newTimeout}). ${sound.initialTimeout}, ${sound.upperBound}`,
+    );
+    audio.play();
 
-  public stopPlaying(soundId: number) {
-    const sound = this.randomSoundsPlaying.find((snd) => snd.id == soundId);
-    if (sound) {
-      this._timeoutManager.clearTimeout(sound.timeoutId);
-      this.randomSoundsPlaying = this.randomSoundsPlaying.filter(
-        (snd) => snd.id != sound.id,
-      );
-    }
-  }
+    updateLoadedSounds((draft) => {
+      const draftSound = draft.find((snd) => snd.id == soundId);
+      if (draftSound == null) {
+        return;
+      }
 
-  public pauseSound(soundId: number) {
-    const sound = this.getSound(soundId);
+      draftSound.timeoutId = timeoutId;
+      draftSound.initialTimeout = newTimeout;
+    });
+  };
 
-    if (sound == null) {
+  const startSound = (soundId: number, timeout: number): number => {
+    const newId = timeoutManager.setTimeout(() => {
+      queuePlay(soundId);
+    }, timeout);
+
+    return newId;
+  };
+
+  const addSound = (url: string, upperBound: number): number => {
+    const newId =
+      loadedSounds.reduce((maxId, sound) => Math.max(maxId, sound.id), 0) + 1;
+
+    const sound: RandomSound = {
+      id: newId,
+      url: url,
+      upperBound,
+      initialTimeout: undefined,
+      pauseInformation: { isPaused: false, remainingTime: 0 },
+      timeoutId: undefined,
+    };
+
+    updateLoadedSounds((draft) => {
+      draft.push(sound);
+    });
+    queuePlay(newId);
+
+    return newId;
+  };
+
+  const removeSound = (soundId: number) => {
+    updateLoadedSounds((draft) => {
+      const sound = draft.find((snd) => snd.id == soundId);
+      if (sound) {
+        if (!sound.pauseInformation.isPaused && sound.timeoutId) {
+          timeoutManager.clearTimeout(sound.timeoutId);
+        }
+        return draft.filter((snd) => snd.id != sound.id);
+      }
+    });
+  };
+
+  const pauseSound = (soundId: number) => {
+    updateLoadedSounds((draft) => {
+      const sound = draft.find((snd) => snd.id == soundId);
+
+      if (sound == null || !sound.timeoutId) {
+        return;
+      }
+
+      const triggerTime = timeoutManager.getTriggerTime(sound.timeoutId);
+      if (triggerTime == null) {
+        return;
+      }
+
+      const remainingTime = triggerTime.getTime() - Date.now();
+      timeoutManager.clearTimeout(sound.timeoutId);
+
+      sound.pauseInformation.remainingTime = remainingTime;
+      sound.pauseInformation.isPaused = true;
+    });
+  };
+
+  const resumeSound = (soundId: number) => {
+    const sound = loadedSounds.find((snd) => snd.id == soundId);
+    if (sound == null || !sound.pauseInformation.isPaused) {
       return;
     }
+    const newTimeoutId = startSound(
+      sound.id,
+      sound.pauseInformation.remainingTime,
+    );
+    updateLoadedSounds((draft) => {
+      const draftSound = draft.find((snd) => snd.id == soundId);
 
-    const triggerTime = this._timeoutManager.getTriggerTime(sound.timeoutId);
-    if (triggerTime == null) {
-      return;
-    }
+      if (draftSound == null || !draftSound.pauseInformation.isPaused) {
+        return;
+      }
 
-    const remainingTime = triggerTime.getTime() - Date.now();
-    this.randomSoundsPaused.push({ ...sound, remainingTime: remainingTime });
-    this.stopPlaying(sound.id);
-  }
+      draftSound.pauseInformation.isPaused = false;
 
-  public resumePlayingSound(soundId: number) {}
+      draftSound.timeoutId = newTimeoutId;
+    });
+  };
 
-  private getTimeout(sound: RandomSound) {
-    return Math.floor(Math.random() * sound.upperBound);
-  }
-}
+  const getTimeout = (upperBound: number) => {
+    return Math.floor(Math.random() * upperBound);
+  };
 
-export const RandomSoundsContext = createContext(
-  new RandomSoundsManager(new TimeoutManager()),
-);
-const TimeoutProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  return { loadedSounds, addSound, removeSound, pauseSound, resumeSound };
+};
+
+export const RandomSoundsContext = createContext({
+  loadedSounds: [],
+  addSound: () => 0,
+  removeSound: () => {},
+  pauseSound: () => {},
+  resumeSound: () => {},
+} as SoundsProviderValues);
+
+const SoundsProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const timeoutContext = useContext(TimeoutContext);
-  const timeoutManager = useRef(new RandomSoundsManager(timeoutContext));
+  const soundsHook = UseRandomSounds(timeoutContext);
 
   return (
-    <RandomSoundsContext.Provider value={timeoutManager.current}>
+    <RandomSoundsContext.Provider value={{ ...soundsHook }}>
       {children}
     </RandomSoundsContext.Provider>
   );
 };
+export default SoundsProvider;
--- src/types/Contexts.ts
+++ src/types/Contexts.ts
@@ -1,3 +1,19 @@
+/** Represents a random sound timeout */
+export interface RandomSound {
+  timeoutId?: number;
+  id: number;
+  /** Represents the media type url to play when triggered */
+  url: string;
+  /** Represents the interval set when sound was queued. */
+  initialTimeout?: number;
+  /** Represents the maximum time can go */
+  upperBound: number;
+  pauseInformation: {
+    remainingTime: number;
+    isPaused: boolean;
+  };
+}
+
 export interface PerformanceProviderValues {
   isDebugMode: boolean;
   setIsDebugMode: React.Dispatch<React.SetStateAction<boolean>>;
@@ -7,4 +23,12 @@ export interface PerformanceProviderValues {
 export interface StylingProviderValues {
   navbarClassOverrides: string;
   setNavbarClassOverrides: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export interface SoundsProviderValues {
+  loadedSounds: RandomSound[];
+  addSound: (url: string, upperBound: number) => number;
+  removeSound: (soundId: number) => void;
+  pauseSound: (soundId: number) => void;
+  resumeSound: (soundId: number) => void;
 }
